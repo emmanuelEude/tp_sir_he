6a8fa06179b8213feea170637a42756638cc8365
Emmanuel Eude <eudeemmanuelassogba@gmail.com>
2024-01-19 16:10:09 +0100

    add javadoc to Main class

--- src/main/java/fr/istic/mdi/ci/Main.java
+++ src/main/java/fr/istic/mdi/ci/Main.java
@@ -1,6 +1,18 @@
 package fr.istic.mdi.ci;
 
+/**
+ * La classe Main est la classe principale du programme, contenant la méthode main.
+ * Elle affiche simplement "Hello world!" sur la console.
+ *
+ * @author Votre Nom
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * La méthode principale (main) du programme.
+     *
+     * @param args Les arguments de la ligne de commande (non utilisés dans cet exemple).
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
