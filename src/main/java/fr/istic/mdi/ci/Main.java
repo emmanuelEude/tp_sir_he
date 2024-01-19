e5f730273571a932400c1e5be404a4f2645ea4e6
Emmanuel Eude <eudeemmanuelassogba@gmail.com>
2024-01-19 16:14:03 +0100

    add format code in Main class

--- src/main/java/fr/istic/mdi/ci/Main.java
+++ src/main/java/fr/istic/mdi/ci/Main.java
@@ -2,6 +2,9 @@ package fr.istic.mdi.ci;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world");
+
     }
 }
+
